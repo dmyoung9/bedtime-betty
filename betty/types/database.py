9fd1868f545bb0e088eb293afac338c543683947
David Young <dmyoung9@asu.edu>
2023-06-13 15:48:37 -0700

    :fire: - Clear out old stuff

--- betty/types/database.py
+++ betty/types/database.py
@@ -11,15 +11,8 @@ class Items(db.Model):
     id: Mapped[int] = db.Column(db.Integer, primary_key=True)
 
 
-# class Images(Items):
-#     url: Mapped[str] = db.Column(db.String, unique=True)
-#     page_id: Mapped[int] = db.Column(db.ForeignKey("pages.id"))
-#     page: Mapped[Sections] = db.relationship(back_populates="image")
-
-
 class Sections(Items):
     content: Mapped[str] = db.Column(db.String, unique=True)
-    # image: Mapped[Images] = db.relationship(back_populates="page")
     story_id: Mapped[int] = db.Column(db.ForeignKey("stories.id"))
 
 
